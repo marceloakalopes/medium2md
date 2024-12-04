3a3de6191dc01c16875b0436d861cd7034aafa2e
marceloakalopes <marcelo.lopes2001@hotmail.com>
2024-12-04 17:02:48 -0500

    refactor: remove unused font import in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Inter from "next/font";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 
