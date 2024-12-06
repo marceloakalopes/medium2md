7c454125769e78791615ac6731e5e4029d7e7455
marceloakalopes <marcelo.lopes2001@hotmail.com>
2024-12-06 12:16:18 -0500

    feat: add animations to HomePage elements and create robots.txt file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -23,6 +23,33 @@ export default function HomePage() {
     }
   };
 
+  useEffect(() => {
+    const heading = document.getElementById("heading");
+    const paragraph = document.getElementById("paragraph");
+    const urlDiv = document.getElementById("urlDiv");
+    const github = document.getElementById("github");
+
+    setTimeout(() => {
+      heading?.classList.add("opacity-100");
+      heading?.classList.remove("translate-y-44");
+    }, 50);
+
+    setTimeout(() => {
+      paragraph?.classList.add("opacity-100");
+      paragraph?.classList.remove("translate-y-44");
+    }, 200);
+
+    setTimeout(() => {
+      urlDiv?.classList.add("opacity-100");
+      urlDiv?.classList.remove("translate-y-44");
+    }, 400);
+
+    setTimeout(() => {
+      github?.classList.add("opacity-100");
+      github?.classList.remove("translate-y-44");
+    }, 600);
+  }, []);
+
   return (
     <div className="relative flex justify-center items-center min-h-screen bg-gray-50">
       {/* Hero Section */}
@@ -32,8 +59,10 @@ export default function HomePage() {
         {/* Main Content */}
         <div className="flex gap-10 max-sm:gap-5  flex-col items-center justify-center max-w-3xl text-center">
           <Link
+            id="github"
             target="_blank"
             href="https://github.com/marceloakalopes/medium2md"
+            className="opacity-0 translate-y-44 duration-700 transition-all"
           >
             <span className="relative inline-block overflow-hidden rounded-full p-[1px]">
               <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#a9a9a9_0%,#0c0c0c_50%,#a9a9a9_100%)] dark:bg-[conic-gradient(from_90deg_at_50%_50%,#171717_0%,#737373_50%,#171717_100%)]" />
@@ -49,14 +78,26 @@ export default function HomePage() {
               </div>
             </span>
           </Link>
-          <h1 className="text-6xl max-sm:text-4xl font-medium tracking-tight text-gray-900">
-            The easiest way to convert medium articles to markdown
+          <h1
+            id="heading"
+            className="text-6xl max-sm:text-4xl font-medium tracking-tight text-gray-900 opacity-0 translate-y-44 duration-500 transition-all"
+          >
+            The easiest way to convert medium articles to{" "}
+            <span className="animate-text-gradient inline-flex bg-gradient-to-r from-neutral-900 via-slate-500 to-neutral-600 bg-[200%_auto] bg-clip-text leading-tight text-transparent dark:from-neutral-300 dark:via-slate-400 dark:to-neutral-400">
+              Markdown
+            </span>
           </h1>
-          <p className="text-lg max-sm:text-md max-w-xl font-medium text-gray-600">
+          <p
+            id="paragraph"
+            className="text-lg max-sm:text-md max-w-xl font-medium text-gray-600 opacity-0 translate-y-44 duration-700 transition-all"
+          >
             Convert medium articles to markdown in seconds. Just paste the URL
             of the article and click convert. It&apos;s that simple.
           </p>
-          <div className="max-w-md mx-auto flex flex-col sm:flex-row gap-2">
+          <div
+            id="urlDiv"
+            className="max-w-md mx-auto flex flex-col sm:flex-row gap-2 opacity-0 translate-y-44 duration-700 transition-all"
+          >
             <Input
               className="min-w-96 bg-white border-gray-400 text-black h-10"
               type="email"
